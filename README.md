7958a8d5dc7c564e5c17f3f8df14868441282599
Bellizoca <isabelli.r.souza@aluno.senai.br>
2026-06-02 09:19:41 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# frontend_store
-Projeto de refatoração de código loja.
+# cadastroprodutosb7
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./*.html', './assets/js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1D4FFE'
+      }
+    }
+  },
+  plugins: []
+};
